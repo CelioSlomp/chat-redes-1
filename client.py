041d7b6b4d6bccb99edd7a8f6b65cd319ba7d140
CelioSlomp <slompcelio132@gmail.com>
2024-06-17 15:15:30 -0300

    melhorado conexao client x server

--- client.py
+++ client.py
@@ -1,30 +1,48 @@
-import socket
-def client(host = 'localhost', port=8082): 
-    # Create a TCP/IP socket 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    # Connect the socket to the server 
-    server_address = (host, port) 
-    print ("Connecting to %s port %s" % server_address) 
-    sock.connect(server_address) 
-    # Send data 
-    try: 
-        # Send data 
-        message = "Test message. This will be echoed" 
-        print ("Sending %s" % message) 
-        sock.sendall(message.encode('utf-8')) 
-        # Look for the response 
-        amount_received = 0 
-        amount_expected = len(message) 
-        while amount_received < amount_expected: 
-            data = sock.recv(16) 
-            amount_received += len(data) 
-            print ("Received: %s" % data) 
-    except socket.error as e: 
-        print ("Socket error: %s" %str(e)) 
-    except Exception as e: 
-        print ("Other exception: %s" %str(e)) 
-    finally: 
-        print ("Closing connection to the server") 
-        sock.close() 
+import socket 
 
-client()
+def main():
+    ipDestino = "localhost"
+    portaDestino = 8082
+
+    while input("Deseja enviar uma mensagem? (S/N): ") == "S":
+        print("---------------------------")
+        conexao = criarConexao(ipDestino, portaDestino) # Sempre precisa fazer isto.
+        print("---------------------------")
+        try:
+            mensagem = input("Digite a mensagem que deseja enviar: ")
+            conexao.sendall(mensagem.encode('utf-8'))
+            
+            msgRecebida = ""
+            qtdRecebida = 0 
+            msgEsperada = len(mensagem) 
+            while qtdRecebida < msgEsperada: 
+                pacote = conexao.recv(16) # Recebe 16 bytes de pacote
+                qtdRecebida += len(pacote)
+                msgRecebida += pacote.decode('utf-8') # Decodifica para verificacao
+                if len(pacote) == 0:
+                    break
+          
+            if msgRecebida == mensagem: # Apenas para feedback do usuario
+                print("Mensagem recebida.")
+            else:
+                print("Mensagem nao recebida. Fechando conexao.")
+
+        except socket.error as e: 
+            print ("Erro no socket", e) 
+        except Exception as e: 
+            print ("Outro erro", e)
+        
+        print("---------------------------")
+    else:
+        print("Fechando conexao.")
+        conexao.close()
+
+def criarConexao(host, porta):
+    conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f"Conectando ao IP {host} com porta {porta}")
+    conexao.connect((host, porta)) 
+
+    return conexao
+
+if __name__ == "__main__":
+    main()
