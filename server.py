10e193fd819ee3d53a0b3c6ba8a07ef9fef53bfe
Matheus Marchi Moro <71672437+mathsmm@users.noreply.github.com>
2024-06-26 18:56:28 -0300

    alterações

--- server.py
+++ server.py
@@ -1,43 +1,44 @@
 import socket
 import threading
+import time
 
-clients = [] # Lista de clientes
-data_payload = 2048 # O payload máximo de dados para ser recebido em 'uma tacada só'
-s: socket.socket = None
-
-def set_standard_socket():
-    s = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-    host = socket.gethostname()
-    port = 22222
-    s.bind((host, port))
-    return s, host, port
-
-def handle_client(client):
-    while True:
-        try:
-            msg = client.recv(2048)
-        except:
-            remove_client(client)
-            break
-
-def remove_client(client):
-    clients.remove(client)
 
 def server():
-    set_standard_socket()
-    s.listen() # Espera pela conexão de qualquer cliente
+    clients = [] # Lista de clientes
+    data_payload = 2048 # O payload máximo de dados para ser recebido em 'uma tacada só'
+
+    def set_standard_socket():
+        s = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
+        host = socket.gethostname()
+        port = 22222
+        s.bind((host, port))
+        return s, host, port
+
+    def handle_client(client):
+        while True:
+            try:
+                msg = client.recv(2048)
+            except:
+                remove_client(client)
+                break
+
+    def remove_client(client: socket.socket):
+        clients.remove(client)
+        client.close() # Testar
+
+    s, host, port = set_standard_socket() # Inicia o socket
+    print(f"Server started at {host}:{port}")
+    start_time = time.time()
+    s.listen(5) # Espera pela conexão de qualquer cliente
     # i = 0
     while True:
         client, addr = s.accept()
         clients.append(client)
-        print(f'Cliente conectado. IP: {addr}')
+        print(f'Client connected. Address: {addr}')
 
         # Inicia uma nova thread para lidar com as mensagens do cliente
         thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
-
-def close_server():
-    s.close()
 
 def main():
     server()
